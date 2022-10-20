66d4404acd52df21fde36f615c9122ffe90dc9ce
Natalia59 <maxi77@bk.ru>
2022-10-21 01:50:42 +0200

    Домашнее задание

--- Program.cs
+++ Program.cs
@@ -1,32 +1,41 @@
-﻿Console.Clear();
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 0,5)
 
-Console.Write($"Введи число М(количество чисел): ");
-int m = Convert.ToInt32(Console.ReadLine());
-int[] massiveNumbers = new int[m];
+Console.Clear();
 
-void InputNumbers(int m)
+static int GetNumberFromUser(string message, string errorMessage)
 {
-for (int i = 0; i < m; i++)
+  while (true)
   {
-    Console.Write($"Введи {i+1} число: ");
-    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    Console.Write(message);
+    bool isCorrect = int.TryParse(Console.ReadLine() ?? "", out int userNumber );
+    if (isCorrect)
+    return userNumber;
+    Console.WriteLine(errorMessage);
   }
 }
 
-
-int Comparison(int[] massiveNumbers)
+static double GetXCoefficients(int A, int B, int C, int D)
 {
-  int count = 0;
-  for (int i = 0; i < massiveNumbers.Length; i++)
-  {
-    if(massiveNumbers[i] > 0) count += 1; 
-  }
-  return count;
+  double x = (double) (C - A) / (B - D);
+  return x;
 }
 
-InputNumbers(m);
+static double GetYCoefficients(int A, int B, double xCoefficients)
+{
+  double y = B * xCoefficients + A;
+  return y;
+}
+int NumberB1 = GetNumberFromUser("Enter b1 : ", "Error. Try again.");
+int NumberK1 = GetNumberFromUser("Enter k1 : ", "Error. Try again.");
+int NumberB2 = GetNumberFromUser("Enter b2 : ", "Error. Try again.");
+int NumberK2 = GetNumberFromUser("Enter k2 : ", "Error. Try again.");
 
-Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
+double x = GetXCoefficients(NumberB1, NumberK1, NumberB2, NumberK2);
+double y = GetYCoefficients(NumberB1, NumberK1, x);
+
+Console.Write($"\nb1 = {NumberB1}, k1 = {NumberK1}, b2 = {NumberB2}, k2 = {NumberK2} ответ: -> ({x};{y})\n");
 
 
   
